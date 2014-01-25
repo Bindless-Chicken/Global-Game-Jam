dd6b7d9da11a072b2f9a5279b5f8569c66fad673
rodyll <seb.m18@gmail.com>
2014-01-25 02:39:55 +0100

    Add COLORS, Player and Type.

---
+++ src/Player.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Seb on 25/01/14.
+ */
+
+
+var Player = new Class
+({
+
+    initialize: function (col) {
+        this.sprite = game.add.sprite();
+        this.type = new Type(col);
+
+    },
+
+    move: function () {
+
+    }
+
+
+
+});
+
+
