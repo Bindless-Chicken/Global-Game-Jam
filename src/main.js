8b6d5dd9edab0840b75cd40b1ffde8d70c16d247
nobe4 <haffreinguev@gmail.com>
2014-01-25 02:43:51 +0100

    added move function used to move the player, some changes may be needed once it will be in the player class

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ loadFiles();
 
 window.onload=function(){
     mainPhaser();
-}
+};
 
 // JS file preloader
 function loadFiles(){
@@ -22,13 +22,43 @@ function loadFiles(){
 
 function mainPhaser(){
     var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+    var player,inputs;
 
     function preload() {
+        game.load.image('h_red','img/h_red.png');
+        game.camera.follow(player);
     }
 
     function create() {
+        player = game.add.sprite(0,0,'h_red');
+        inputs = game.input.keyboard.createCursorKeys(); // bind the keyboard/mouse to inputs
     }
 
     function update () {
+        move(player,inputs);
+        
     }
 }
+
+// ################ TO MOVE ! ##################
+// function to put in the player class
+function move(player,inputs){
+    var speed = 200; // to replace by the field in the player class
+
+    if (inputs.left.isDown)
+            player.body.velocity.x = -speed;
+        else if (inputs.right.isDown)
+            player.body.velocity.x = speed;
+        else
+            player.body.velocity.x = (player.body.velocity.x < 0)?0:player.body.velocity.x - 5;
+
+        if (inputs.up.isDown)
+            player.body.velocity.y = -speed;
+        else if (inputs.down.isDown)
+            player.body.velocity.y = speed;
+        else
+            player.body.velocity.y = (player.body.velocity.y < 0)?0:player.body.velocity.y - 5;
+}
+
+
+
